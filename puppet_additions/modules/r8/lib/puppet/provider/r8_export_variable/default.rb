e4fd227bfbb25ffac8e9d5b64620b7e97246469e
bjusufbe <bakir@atlantbh.com>
2016-06-21 16:06:44 +0200

    added type/provider for r8_export_variable custom resource

---
+++ puppet_additions/modules/r8/lib/puppet/provider/r8_export_variable/default.rb
@@ -0,0 +1,27 @@
+require 'fileutils'
+
+Puppet::Type.type(:r8_export_variable).provide(:default) do
+  desc "r8 export variable"
+
+  def create
+    name = resource[:name]
+    content = resource[:content]
+    if name =~ /(^.+)::(.+$)/
+      component = $1
+      attribute = $2
+      if content = (content == '***' ? scope.lookupvar(name) : content)
+        p = Thread.current[:exported_variables] ||= Hash.new
+        (p[component] ||= Hash.new)[attribute] = content
+        File.open('/tmp/dtk_exported_variables', 'w') { |f| f.write(Marshal.dump(p)) }
+      end
+    end
+  end
+
+  def destroy
+    FileUtils.rm_rf("/tmp/dtk_exported_variables")
+  end
+
+  def exists?
+    File.file?("/tmp/dtk_exported_variables")
+  end
+end
