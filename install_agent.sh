e29e2a1e84264146478dccad411ef276c4b24f29
adnanabh <adnan.muslija@atlantbh.com>
2016-08-10 14:52:00 +0200

    fix docker installation for amazon vms

--- install_agent.sh
+++ install_agent.sh
@@ -90,7 +90,11 @@ dtk-node-agent -d
 [[ ! -f /etc/logrotate.d/puppet ]] && cp ${base_dir}/src/etc/logrotate.d/puppet /etc/logrotate.d/puppet
 
 # install docker
-curl -sSL https://get.docker.com/ | sh
+if [[ ${osname} == 'Amazon' ]];then
+	yum update -y && yum install docker -y;
+else
+  curl -sSL https://get.docker.com/ | sh
+fi
 
 # remove root ssh files
 rm -f /root/.ssh/id_rsa
